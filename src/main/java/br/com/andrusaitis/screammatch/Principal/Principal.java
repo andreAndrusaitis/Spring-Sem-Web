b492de7ad299df7c59305d1ccd95fc310d4d87e6
andreAndrusaitis <andreandrusaitis@gmail.com>
2024-11-07 16:00:50 -0300

    adicionando datas e novos requisitores

--- src/main/java/br/com/andrusaitis/screammatch/Principal/Principal.java
+++ src/main/java/br/com/andrusaitis/screammatch/Principal/Principal.java
@@ -3,12 +3,17 @@ package br.com.andrusaitis.screammatch.Principal;
 import br.com.andrusaitis.screammatch.model.DadosEpisodio;
 import br.com.andrusaitis.screammatch.model.DadosSerie;
 import br.com.andrusaitis.screammatch.model.DadosTemporada;
+import br.com.andrusaitis.screammatch.model.Episodio;
 import br.com.andrusaitis.screammatch.service.ConsumoApi;
 import br.com.andrusaitis.screammatch.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -52,5 +57,43 @@ public class Principal {
 //            }
 //        }
         temporadas.forEach(t -> t.episodios().forEach(e-> System.out.println(e.titulo())));
+
+        //Criando lista para trabalhar com Strems
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())//Fluxo de dados dos episodios das temporadas
+                .collect(Collectors.toList());//Coloca a lista de ep dentro da lista dadosEp
+
+        System.out.println("\n Top 5 Episódios");
+        dadosEpisodios.stream()
+                .filter(e->!e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+        //Lista de episodios que transforma os Eps de cada temporada
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d)))
+                .collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        //Colocando Scam para filtar ano de visualização dos episodios
+        System.out.println("A partir de que ano, você gostaria de ver os Episodios?");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano,1,1);
+
+        //Formatador de datas
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada()
+                        + " Episódio: " + e.getTitulo()
+                        + " Data de lançamento: " + e.getDataLancamento().format(formatador)
+                ));
     }
 }
---
+++ src/main/java/br/com/andrusaitis/screammatch/model/Episodio.java
@@ -0,0 +1,82 @@
+package br.com.andrusaitis.screammatch.model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.zip.DataFormatException;
+
+public class Episodio {
+    private Integer temporada;
+    private String titulo;
+    private Integer numeroEpisodio;
+    private double avaliacao;
+    private LocalDate dataLancamento;
+
+    public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
+        this.temporada = numeroTemporada;
+        this.titulo = dadosEpisodio.titulo();
+        this.numeroEpisodio = dadosEpisodio.numero();
+
+        //Tratando erro de datas que não estão no formato adequado
+        try {
+            this.dataLancamento = LocalDate.parse(dadosEpisodio.dataLancamento());
+        }catch (DateTimeParseException ex){
+            this.dataLancamento = null;
+        }
+
+        //Tratando erros de avaliações com N/A
+        try {
+            this.avaliacao = Double.valueOf(dadosEpisodio.avaliacao());
+        } catch (NumberFormatException ex){
+            this.avaliacao = 0.0;
+        }
+    }
+
+    public Integer getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getNumeroEpisodio() {
+        return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(Integer numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    @Override
+    public String toString() {
+        return  "temporada=" + temporada +
+                ", titulo='" + titulo + '\'' +
+                ", numeroEpisodio=" + numeroEpisodio +
+                ", avaliacao=" + avaliacao +
+                ", dataLancamento=" + dataLancamento;
+    }
+}
